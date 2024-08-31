44787b594411107d65e809124842783999f7bbf5
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-08-31 14:42:34 -0300

    feat/new-components, novos componentes feitos e so o project em andamento.

---
+++ src/components/Banner.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import "./Banner.css";
+import { Col, Container, Row } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+  const toRotate = ["Developer", "Backend", "Frontend", "Designer"];
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={5}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>
+              {`Hi! I'm Leonardo Adami `} <br />
+              <span className="wrap">Backend Junior Developer</span>
+              <p>
+                I'm always looking for new challenges and new experiences. I'm a
+                self-taught developer.
+                <button onClick={() => console.log("connect")}>
+                  <ArrowRightCircle size={25} /> Let's connect
+                </button>
+              </p>
+            </h1>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
